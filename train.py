aa34efe13adbeb4d63f57421ee0d2d1c035d0fca
mugan <haebing25@gmail.com>
2023-03-01 21:52:09 +0900

    해결했다

--- train.py
+++ train.py
@@ -10,6 +10,7 @@ from tqdm import tqdm
 class Trainer:
     
     def __init__(self, model, loss_fn, optimizer, epochs, batch_size):
+        
         self.model = model
         self.loss_fn = loss_fn
         self.optimizer = optimizer
@@ -28,10 +29,10 @@ class Trainer:
             loss = self.loss_fn(y, logits)
         grads = tape.gradient(loss, self.model.trainable_weights)
         self.optimizer.apply_gradients(zip(grads, self.model.trainable_weights))
-    
+        
         return loss, logits
     
-    def train(self, train_ds, val_ds, steps_per_epoch, val_step, acc_metric, checkpoint_manager) :
+    def train(self, train_ds, steps_per_epoch, val_ds, val_steps, checkpoint_manager) :
         
         metrics_names = ['train_loss', 'train_acc', 'val_loss', 'val_acc']
         history = {'train_loss': [], 'train_acc': [], 'val_loss': [], 'val_acc': []}
@@ -39,57 +40,74 @@ class Trainer:
         best_loss = 100
         for epoch in range(self.epochs):
             
+            # train_ds = train_ds.shuffle(100)
+            # val_ds = val_ds.shuffle(100)
+
+            train_ds = train_ds.take(steps_per_epoch)
+            val_ds = val_ds.take(val_steps)
+
+            train_loss = tf.keras.metrics.Mean(name='train_loss')
+            train_accuracy = tf.keras.metrics.SparseCategoricalAccuracy(name='train_accuracy')
+
+            valid_loss = tf.keras.metrics.Mean(name='valid_loss')
+            valid_accuracy = tf.keras.metrics.SparseCategoricalAccuracy(name='valid_accuracy')
+
+            progBar = Progbar(steps_per_epoch * self.batch_size, stateful_metrics=metrics_names)
             print("\nStart of epoch %d" % (epoch,))
             
             start_time = time.time()
-            train_loss = tf.keras.metrics.Mean()
-            train_acc = tf.keras.metrics.SparseCategoricalAccuracy()
-        
-            progBar = Progbar(steps_per_epoch * self.batch_size, stateful_metrics=metrics_names)
-            # pbar_train = tqdm(total=steps_per_epoch)
+            
+            # # progBar = Progbar(steps_per_epoch * self.batch_size, stateful_metrics=metrics_names)
+            # # pbar_train = tqdm(total=steps_per_epoch)
             for step_train, (x_batch, y_batch) in enumerate(train_ds):
-                batch_loss, logits = self.train_step(x_batch, y_batch)
-                batch_acc = self.compute_acc(logits, y_batch)
-                # train_loss(batch_loss)
-                # train_acc(y_batch, self.model(x_batch))
-                # pbar_train.set_description(f'train_loss: {train_loss.result():.4f} - train_acc: {train_acc.result():.4f}')
-                # pbar_train.update()
-                # values = [('train_loss', train_loss.result().numpy()), ('train_acc', train_acc.result().numpy())]
-                values = [('train_loss', batch_loss), ('train_acc', batch_acc)]
+                loss, logits = self.train_step(x_batch, y_batch)
+                # train_acc = self.compute_acc(logits, y_batch)
+                train_loss(loss)
+                train_acc()
+            #     # values = [('train_loss', train_loss.result().numpy()), ('train_acc', train_acc.result().numpy())]
+                values = [('train_loss', train_loss), ('train_acc', train_acc)]
                 progBar.update((step_train + 1) * self.batch_size, values=values)
                 
-            # history['train_loss'].append(train_loss.result().numpy())
-            # history['train_acc'].append(train_acc.result().numpy())
+                # if step_train % 100 == 0:
+                #     print("Epoch: {}/{}, step: {}/{}, loss: {:.5f}, accuracy: {:.5f}".format(epoch + 1,
+                #                                                                         self.epochs,
+                #                                                                         step_train,
+                #                                                                         math.ceil(train_size / self.batch_size),
+                #                                                                         train_loss.result(),
+                #                                                                         train_acc.result()))
+            # history['train_loss'].append(train_loss)
+            # history['train_acc'].append(train_acc)
             # print(f"train_loss: {history['train_loss'][-1]:.4f} - train_acc: {history['train_acc'][-1]:.4f}")
  
             # pbar_train.close()
-            if val_ds is not None:
-                val_loss = tf.keras.metrics.Mean()
-                val_acc = tf.keras.metrics.SparseCategoricalAccuracy()
+            # if val_ds is not None:
+            #     val_loss = tf.keras.metrics.Mean()
+            #     val_acc = tf.keras.metrics.SparseCategoricalAccuracy()
                 
-                for step_val, (x_batch, y_batch) in enumerate(val_ds):
-                    logits = model(x_batch, training=False)
+            #     for step_val, (x_batch, y_batch) in enumerate(val_ds):
+            #         logits = model(x_batch, training=False)
                     
-                    batch_val_loss = self.loss_fn(y_batch, logits)
-                    batch_val_acc = self.compute_acc(logits, y_batch)
+            #         # batch_val_loss = self.loss_fn(y_batch, logits)
+            #         # batch_val_acc = self.compute_acc(logits, y_batch)
                     
-                    val_loss(self.loss_fn(y_batch, logits))
-                    val_acc(y_batch, self.model(x_batch))
+            #         val_loss(self.loss_fn(y_batch, logits))
+            #         val_acc(y_batch, self.model(x_batch))
                     
-                    values = [('train_loss', batch_loss), ('train_acc', batch_acc), ('val_loss', batch_val_loss), ('val_acc', batch_val_acc)]
-                progBar.update((step_train + 1) * self.batch_size, values=values, finalize=True)   
+            #     #     values = [('train_loss', batch_loss), ('train_acc', batch_acc), ('val_loss', batch_val_loss), ('val_acc', batch_val_acc)]
+            #     # progBar.update((step_train + 1) * self.batch_size, values=values, finalize=True)   
                 
-                history['val_loss'].append(val_loss.result().numpy())
-                history['val_acc'].append(val_acc.result().numpy())
-                print(f"val_loss: {history['val_loss'][-1]:.4f} - val_acc: {history['val_acc'][-1]:.4f}")
+            #     history['val_loss'].append(val_loss.result().numpy())
+            #     history['val_acc'].append(val_acc.result().numpy())
+            #     print(f"val_loss: {history['val_loss'][-1]:.4f} - val_acc: {history['val_acc'][-1]:.4f}")
             
-            print("Time taken: %.2fs" % (time.time() - start_time))
+            # print("Time taken: %.2fs" % (time.time() - start_time))
             
-            if batch_val_loss < best_loss:
-                best_loss = batch_val_loss
-                print("\nSave better model: ", end='')
-                print(checkpoint_manager.save())
-        return history 
+            # if val_loss < best_loss:
+            #     best_loss = val_loss
+            #     print("\nSave better model: ", end='')
+            #     print(checkpoint_manager.save())
+                
+        # return history 
             
 if __name__ == "__main__" :
     
@@ -99,10 +117,7 @@ if __name__ == "__main__" :
     
     loss_function = tf.keras.losses.SparseCategoricalCrossentropy()
     optimizer = tf.keras.optimizers.Adam()
-    train_acc_metric = tf.keras.metrics.SparseCategoricalAccuracy()
-    val_acc_metric = tf.keras.metrics.SparseCategoricalAccuracy()
-    acc_metric = tf.keras.metrics.SparseCategoricalAccuracy()
-    
+
     model = CNN(num_classes=10)
     epochs = 10
     batch_size = 32
@@ -116,4 +131,6 @@ if __name__ == "__main__" :
     
     # Trainer 클래스를 사용하여 모델 학습
     trainer = Trainer(model, loss_fn=loss_function, optimizer=optimizer, epochs=epochs, batch_size=batch_size)
-    history = trainer.train(train_ds, val_ds, steps_per_epoch, val_steps, acc_metric, checkpointmanager)
+    trainer.train(train_ds=train_ds, steps_per_epoch=steps_per_epoch, val_steps=val_steps, val_ds=val_ds, checkpoint_manager=checkpointmanager)
+         
+
